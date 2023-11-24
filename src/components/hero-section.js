d039c70fed4a95860e738d5a25c822c8060c4d2f
Arif Hussain <arif3337179@gmail.com>
2023-11-24 21:07:47 +0500

    navbar changes

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -2,16 +2,18 @@ import Img from '../assets/imgs/hero-bg.png';
 export default function HeroSection() {
     return (
         <>
-            <div className="conatiner" style={{padding:'40px'}}>
+            <div className="conatiner" style={{ padding: '40px' }}>
                 <div className="row">
-                    <div className=" col-sm-12 col-lg-6 col-md-6">
-                        <h1>Arif Hussain</h1>
+                    <div className=" col-sm-12 col-lg-12 col-md-12 text-center">
+                        <h1 className="dancing-script">Arif Hussain</h1>
                         <h3>Full Stack Developer</h3>
                         <p>Hi, I am a Full Stack Developer. I have 2 years of experience in developing
                             web applications using ReactJS, NodeJS, ExpressJS, MongoDB, MySQL, HTML, CSS, Bootstrap, Material UI, JavaScript, jQuery, AJAX, JSON, REST API, Git, GitHub, Heroku, Netlify, and Firebase.
                         </p>
                     </div>
-                    <div className=" col-sm-12 col-lg-6 col-md-6">
+                </div>
+                <div className="row">
+                    <div className=" col-sm-12 col-lg-12 col-md-12">
                         <img src={Img} alt="Arif Hussain" className="img-fluid hero-img" />
                     </div>
                 </div>
