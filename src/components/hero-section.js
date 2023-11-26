8bb1720b079ffebf32a1e80d6ca44bbeef97e25a
Arif Hussain <arif3337179@gmail.com>
2023-11-26 19:58:01 +0500

    Hero Section

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -1,20 +1,26 @@
 import Img from '../assets/imgs/logo-img.jpeg';
+import Img2 from '../assets/imgs/hero-devices.svg';
 export default function HeroSection() {
     return (
         <>
-            <div className="conatiner" style={{ padding: '40px', color:'white'}}>
+            <div className="conatiner" style={{ padding: '40px', color: 'white' }}>
                 <div className="row">
                     <div className=" col-sm-12 col-lg-12 col-md-12 text-center">
                         <h1 className="dancing-script">Arif Hussain</h1>
                         <h3>Web Applications Developer</h3>
-                        <p>Hi, I am a Full Stack Developer. I have 2 years of experience in developing
-                            web applications <br /> using ReactJS, NodeJS, ExpressJS, MongoDB, MySQL, HTML, CSS, Bootstrap, Material UI, <br />JavaScript, jQuery, AJAX, JSON, REST API, Git, GitHub, Heroku, Netlify, and Firebase.
+                        <p>
+                            I design and code beautifully simple things, and I love what I do.
                         </p>
                     </div>
                 </div>
-                <div className="row">
+                <div className="row mt-5">
                     <div className=" col-sm-12 col-lg-12 col-md-12  justify-content-center d-flex">
-                        <img src={Img} alt="Arif-Hussain" style={{height:'200px'}} className="img-fluid hero-img"  />
+                        <img src={Img} alt="Arif-Hussain" style={{ height: '150px' }} className="img-fluid hero-img" />
+                    </div>
+                </div>
+                <div className="row mt-5">
+                    <div className=" col-sm-12 col-lg-12 col-md-12  justify-content-center d-flex">
+                        <img src={Img2} alt="Arif-Hussain" style={{ height: '60%' }} className="img-fluid hero-img" />
                     </div>
                 </div>
             </div>
