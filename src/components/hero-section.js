7324d0788949aec30a09ab2a3633fd057e0f11e4
Arif Hussain <arif3337179@gmail.com>
2023-12-23 22:01:18 +0500

    cards

--- src/components/hero-section.js
+++ src/components/hero-section.js
@@ -1,6 +1,8 @@
 import Img from '../assets/imgs/logo-img.jpeg';
 import Img2 from '../assets/imgs/hero-devices.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
+import Card from 'react-bootstrap/Card';
 
 export default function HeroSection() {
     const [text] = useTypewriter({
@@ -47,27 +49,165 @@ export default function HeroSection() {
                 </div>
                 <div className="row mt-5">
                     <div className=" col-sm-12 col-lg-12 col-md-12  justify-content-center d-flex">
-                        <img src={Img} alt="Arif-Hussain" style={{ height: '150px' }} className="img-fluid hero-img" />
+                        <img src={Img} alt="Arif-Hussain" style={{ height: '200px' }} className="img-fluid hero-img" />
                     </div>
                 </div>
             </div>
 
-            <div className="mt-5 d-flex justify-content-center">
-                <img
-                    src={Img2}
-                    alt="Arif-Hussain"
-                    style={{ width: '100%', height: 'auto', maxWidth: '60%' }}
-                    className="img-fluid hero-img"
-                />
+            <div className="row">
+                <div className="col-lg-3 col-md-2  col-sm-1 ">
+
+                </div>
+                <div className="col-lg-6 col-md-8  col-sm-10 mt-5 d-flex justify-content-center">
+                    <img
+                        src={Img2}
+                        alt="Arif-Hussain"
+                        style={{ height: 'auto', maxHeight: '100%' }}
+                        className="img-fluid hero-img"
+                    />
+                </div>
+                <div className="col-lg-3 col-md-2  col-sm-1">
+
+                </div>
             </div>
 
-            <div className="Intro-section-outer">
+
+            <div className="Intro-section-outer ">
                 <div className="Intro-section">
 
-                    <h1>Hi, I’m Arif. Nice to meet you.</h1>
-                    <p>
-                        Since beginning my journey as a freelance designer nearly 10 <br />years ago, I've done remote work for agencies, consulted <br />for startups, and collaborated with talented people to <br />create digital products for both business and consumer <br />use. I'm quietly confident, naturally curious, <br />and perpetually working on improving my chops one design <br />problem at a time.
+                    <h1
+                        style={{
+                            marginTop: '100px',
+                            fontSize: '2.5rem',
+                            fontWeight: '700',
+                            letterSpacing: '1px',
+                            color: 'white',
+                            fontFamily: 'monospace',
+                        }}
+                    >Hi, I’m Arif. Nice to meet you.</h1>
+                    <p
+                        style={{
+                            fontSize: '1.5rem',
+                            fontWeight: '400',
+                            lineHeight: '1.5',
+                            letterSpacing: '1px',
+                            color: 'white',
+                        }}
+                    >
+                        Since beginning my journey as a freelance designer nearly 10 years ago, I've done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
                     </p>
+                </div>
+            </div>
+
+            <div className='container-fluid'
+                style={{ marginTop: '-80px' }}
+            >
+                <div className="row">
+
+                    <div className="col-lg-4 col-md-12 col-sm-12">
+                        <Card style={{ textAlign: 'center', margin: '20px', fontSize: '25px', fontFamily: 'sans-serif' }}>
+                            <Card.Body >
+                                <Card.Title style={{
+                                    fontFamily: 'monospace',
+                                    fontWeight: '600',
+                                    fontSize: '2.5rem',
+                                }}
+                                >Designer</Card.Title>
+                                <br />
+                                <Card.Text>
+                                    I value simple content structure, clean design patterns, and thoughtful interactions.
+                                </Card.Text>
+                                <br />
+                                <Card.Text style={{ color: '#6E07F3' }}>
+                                    Things I enjoy designing:
+                                </Card.Text>
+                                <Card.Text>
+                                    UX, UI, Web, Apps, Logos
+                                </Card.Text>
+                                <br />
+                                <Card.Text style={{ color: '#6E07F3' }}>
+                                    Design Tools:
+                                </Card.Text>
+                                <Card.Text>
+                                    Figma <br />
+                                    Font Awesome <br />
+                                    Sketch <br />
+                                    Webflow <br />
+                                </Card.Text>
+
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-sm-12">
+                        <Card style={{ textAlign: 'center', margin: '20px', fontSize: '25px', fontFamily: 'sans-serif' }}>
+                            <Card.Body >
+                                <Card.Title style={{
+                                    fontFamily: 'monospace',
+                                    fontWeight: '600',
+                                    fontSize: '2.5rem',
+                                }}
+                                >Web App Developer</Card.Title>
+                                <br />
+                                <Card.Text>
+                                    I like to code things from scratch, and enjoy bringing ideas to life in the browser.
+                                </Card.Text>
+                                <br />
+                                <Card.Text style={{ color: '#6E07F3' }}>
+                                    Languages I speak:
+                                </Card.Text>
+                                <Card.Text>
+                                    HTML, CSS, JS, Java, PHP, React
+                                </Card.Text>
+                                <br />
+                                <Card.Text style={{ color: '#6E07F3' }}>
+                                    Dev Tools:
+                                </Card.Text>
+                                <Card.Text>
+                                    Visual Studio Code <br />
+                                    Git <br />
+                                    npm <br />
+                                    Bootstrap <br />
+                                    Vercel <br />
+                                    Notion <br />
+                                </Card.Text>
+
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col-lg-4 col-md-12 col-sm-12">
+                        <Card style={{ textAlign: 'center', margin: '20px', fontSize: '25px', fontFamily: 'sans-serif' }}>
+                            <Card.Body >
+                                <Card.Title style={{
+                                    fontFamily: 'monospace',
+                                    fontWeight: '600',
+                                    fontSize: '2.5rem',
+                                }}
+                                >Web App Developer</Card.Title>
+                                <br />
+                                <Card.Text>
+
+                                </Card.Text>
+                                <br />
+                                <Card.Text style={{ color: '#6E07F3' }}>
+                                    Things I enjoy designing:
+                                </Card.Text>
+                                <Card.Text>
+                                    UX, UI, Web, Apps, Logos
+                                </Card.Text>
+                                <br />
+                                <Card.Text style={{ color: '#6E07F3' }}>
+                                    Design Tools:
+                                </Card.Text>
+                                <Card.Text>
+                                    Figma <br />
+                                    Font Awesome <br />
+                                    Sketch <br />
+                                    Webflow <br />
+                                </Card.Text>
+
+                            </Card.Body>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </>
